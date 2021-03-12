d298e98fef1bab78b7e57e3e56100f13803f36af
kosuke-17 <tamutamu17@icloud.com>
2021-03-12 19:14:42 +0900

    修正完了4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_tweet, only: [:edit , :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update]
+  before_action :set_tweet, only: [:edit , :show, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if  @item.update(item_params)
       redirect_to item_path(@item.id)
     else
